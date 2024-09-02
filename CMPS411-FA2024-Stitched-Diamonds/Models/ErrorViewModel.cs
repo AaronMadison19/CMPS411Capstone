ab548fabc332923e2165ab8bf63ecc354d1083f4
AaronMadison19 <aaron.madison@selu.edu>
2024-09-02 11:20:59 -0500

    Addding local project files

---
+++ CMPS411-FA2024-Stitched-Diamonds/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CMPS411_FA2024_Stitched_Diamonds.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
