a77e4a856121200946ef4bf173e1f2b66fc866f6
WondaMegapon <wondamegapon@gmail.com>
2024-11-14 14:27:08 -0600

    Added 404 pages.

--- CMPS411-FA2024-Stitched-Diamonds/cmps411-fa2024-stitched-diamonds/src/app/components/navbar/index.tsx
+++ CMPS411-FA2024-Stitched-Diamonds/cmps411-fa2024-stitched-diamonds/src/app/components/navbar/index.tsx
@@ -30,7 +30,7 @@ const Navbar = () => {
         <div className="md:hidden flex items-center">
           <button onClick={toggleMenu} className="text-white focus:outline-none">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
             </svg>
           </button>
         </div>
