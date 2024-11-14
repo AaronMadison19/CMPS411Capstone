b8f053d2c0c449f5c789715e13281cc285a0ecce
WondaMegapon <wondamegapon@gmail.com>
2024-11-14 14:59:17 -0600

    Justify Items exists. Also more breakpoints

--- CMPS411-FA2024-Stitched-Diamonds/cmps411-fa2024-stitched-diamonds/src/app/about/page.tsx
+++ CMPS411-FA2024-Stitched-Diamonds/cmps411-fa2024-stitched-diamonds/src/app/about/page.tsx
@@ -68,7 +68,7 @@ const About: React.FC = () => {
           </div>
 
           {/* Developers Section */}
-          <div className="container mx-auto px-9 py-16 grid grid-cols-1 md:grid-cols-5 gap-8">
+          <div className="flex justify-items-center mx-auto py-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
             <div className="bg-white shadow-xl rounded-lg p-6 w-64">
               <img src="https://via.placeholder.com/100" alt="Team Member" className="rounded-full mx-auto mb-4" />
               <h3 className="text-xl font-semibold text-gray-800">Aaron Madison</h3>
